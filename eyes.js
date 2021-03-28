f62e8f330dd06b54afd45b77d71c125dcb67f197
yauntee <77030405+yauntee@users.noreply.github.com>
2021-03-28 18:28:18 -0400

    Initial Project Files

---
+++ eyes.js
@@ -0,0 +1,15 @@
+// Hooks the class ball via the DOM
+const balls = document.getElementsByClassName('ball');
+
+//Changes the position of the eye ball based on mouse position
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+}
+
+};
